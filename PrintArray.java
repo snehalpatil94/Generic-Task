eee783ca9517872a01f90fffb74d77229a2f669c
snehalpatil94 <erpatilsnehal@gmail.com>
2022-04-20 23:43:32 +0530

    Welcome Message

---
+++ PrintArray.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.generictask;
+
+public class PrintArray {
+
+	public static void main(String[] args) {
+		System.out.println("------------------- Welcome to Generic demo ------------------- ");
+	}
+}
