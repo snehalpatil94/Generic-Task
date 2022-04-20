b5173d7e4da7b1887db918f68199452e8ef8fc99
snehalpatil94 <erpatilsnehal@gmail.com>
2022-04-20 23:51:39 +0530

    program to print Integer, Double and Character array

--- PrintArray.java
+++ PrintArray.java
@@ -1,8 +1,51 @@
 package com.bridgelabz.generictask;
 
+/**
+ * Write a program to print Integer, Double and Character
+ * 
+ * @author : Snehal Patil
+ *
+ */
 public class PrintArray {
 
+	// Printing Integer Array
+	private static void toPrint(Integer[] inputArray) {
+		System.out.print("Integer Array : ");
+		for (int i = 0; i < inputArray.length; i++) {
+			System.out.print(inputArray[i] + " ");
+		}
+		System.out.println();
+	}
+
+	// Printing Double Array
+	private static void toPrint(Double[] inputArray) {
+		System.out.print("Double Array : ");
+		for (double element : inputArray) {
+			System.out.printf(element + "  ");
+		}
+		System.out.println();
+	}
+
+	// Printing Character Array
+	private static void toPrint(Character[] inputArray) {
+		System.out.print("Charater Array : ");
+		for (char element : inputArray) {
+			System.out.printf("%s ", element);
+		}
+	}
+
 	public static void main(String[] args) {
-		System.out.println("------------------- Welcome to Generic demo ------------------- ");
+		System.out.println("------------------- Generic demo -------------------- ");
+		System.out.println();
+		
+		// Array initialisation
+		Integer[] intArray = { 2, 4, 6, 8, 10 };
+		Double[] doubleArray = { 1.1, 3.3, 5.5, 7.7 };
+		Character[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+
+		// Print Array
+		toPrint(intArray);
+		toPrint(doubleArray);
+		toPrint(charArray);
 	}
 }
